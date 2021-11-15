7843590e038b0e7573bd29c43aab4848532e9ea9
Arif Hidayat <arif.h@mail.ugm.ac.id>
2021-11-15 11:10:30 +0700

    fix date and add response role

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,19 +5,33 @@ use Tymon\JWTAuth\Exceptions\JWTException;
 use Illuminate\Http\Request;
 use Hash;
 use JWTAuth;
+use Illuminate\Support\Facades\Validator;
 
 
 class AuthController extends Controller
 {
     public function register(UserRequest $request)
     {
-        $credentials = $request->only('name', 'username', 'password','role');
+        $validator = Validator::make($request->all(), [
+            'name'   => 'required',
+            'username' => 'required',
+            'password' => 'required',
+            'departemen_id' => 'required',
+            'role_id' => 'required',
+        ]);
+      
+        //response error validation
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $credentials = $request->only('name', 'username', 'password','role_id','departemen_id');
         $name = $request->name;
         $username = $request->username;
         $password = $request->password;
         $departemen_id = $request->departemen_id;
         $user = User::create(['name' => $name, 'username' => $username, 'password' => Hash::make($password), 'departemen_id' => $departemen_id]);
-        $role = Role::where('id', $request->role)->first();
+        $role = Role::where('id', $request->role_id)->first();
         $user->attachRole($role);
 
         return response()->json(['success'=> true, 'message'=> 'Thanks for signing up!']);
